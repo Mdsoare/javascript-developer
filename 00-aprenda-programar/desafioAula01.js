3b7b471a154f8c6ed6234b02548ee67ef8fa9019
Marcelo Soares <106860930+Mdsoare@users.noreply.github.com>
2023-09-17 20:20:55 -0300

    feat: Adicionando arquivo desafioAula01.js

---
+++ 00-aprenda-programar/desafioAula01.js
@@ -0,0 +1,24 @@
+/*
+AUTOR: Marcelo Soares
+DATA: 17/09/2023
+
+DESAFIO: FAÇA UMA PROGRAMA PARA CALCULAR O VALOR DE UMA VIAGEM. DEVE TER 3 VARIÁVEIS:
+
+1 - DISTÂNCIA EM KM;
+2 - GASTO MÉDIO DE COMBUSTÍVEL DO CARRO EM KM/L;
+3 - PREÇO DO COMBUSTÍVEL;
+
+AO FINAL, IMPRIMA NA CONSOLE O VALOR QUE SERÁ GASTO DE COMBUSTÍVEL PARA ESTA VIAGEM.
+*/
+
+function calculoGastoCobustivel(distanciaEmKm, kmPorLitros, precoCombustivel){
+    // Cálculo:
+    const litrosConsumidos = distanciaEmKm / kmPorLitros;
+    const valorGasto = litrosConsumidos * precoCombustivel;
+
+    // Resultado em 2 casas decimais:
+    console.log('O custo total de combustível para esta viagem será de R$ ' + valorGasto.toFixed(2));
+}
+
+// Chamando a função com valores aleatórios
+calculoGastoCobustivel(97.6, 10, 5.79);
